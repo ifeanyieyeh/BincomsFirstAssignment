048ca942af9385e63acb7887071562cb59a4067c
Ifeanyi <Ifeanyi@IFEANYICHUKWU-E>
2025-10-15 02:52:58 +0100

    Add project files.

---
+++ BincomsFirstAssignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BincomsFirstAssignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
